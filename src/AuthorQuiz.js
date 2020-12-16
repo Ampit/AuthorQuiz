e01a9e4c9078520cc7ca99bbbef386a0d7cd1245
Amarpreet Singh <ampit.xd@gmail.com>
2020-12-16 18:08:46 -0500

    Fixed title and footer spacing

--- src/AuthorQuiz.js
+++ src/AuthorQuiz.js
@@ -8,135 +8,147 @@ import "./bootstrap.min.css";
 import { render } from "@testing-library/react";
 
 function Hero() {
-  return (
-    <div className="jumbotron col-10 offset-1">
-      <h1>Author Quiz</h1>
-      <p>Select the book written by the author shown.</p>
-    </div>
-  );
+    return (
+        <div className="jumbotron col-10 offset-1">
+            <h1>Author Quiz</h1>
+            <p>Select the book written by the author shown.</p>
+        </div>
+    );
 }
 
 function Book({ title, onClick }) {
-  return (
-    <div
-      className="answer"
-      onClick={() => {
-        onClick(title);
-      }}
-    >
-      <h4>{title}</h4>
-    </div>
-  );
+    return (
+        <div
+            className="answer"
+            onClick={() => {
+                onClick(title);
+            }}
+        >
+            <h4>{title}</h4>
+        </div>
+    );
 }
 
 function Turn({ author, books, highlight, onAnswerSelected }) {
-  function highlightToBgColor(highlight) {
-    const mapping = {
-      none: "",
-      correct: "green",
-      wrong: "red"
-    };
-    return mapping[highlight];
-  }
+    function highlightToBgColor(highlight) {
+        const mapping = {
+            none: "",
+            correct: "green",
+            wrong: "red",
+        };
+        return mapping[highlight];
+    }
 
-  return (
-    <div
-      className="row turn"
-      style={{ backgroundColor: highlightToBgColor(highlight) }}
-    >
-      <div className="col-4 offset-1">
-        <img src={author.imageUrl} className="authorimage" alt="Author" />
-      </div>
-      <div className="col-6">
-        {books.map(title => (
-          <Book title={title} key={title} onClick={onAnswerSelected} />
-        ))}
-      </div>
-    </div>
-  );
+    return (
+        <div
+            className="row turn"
+            style={{ backgroundColor: highlightToBgColor(highlight) }}
+        >
+            <div className="col-4 offset-1">
+                <img
+                    src={author.imageUrl}
+                    className="authorimage"
+                    alt="Author"
+                />
+            </div>
+            <div className="col-6">
+                {books.map(title => (
+                    <Book
+                        title={title}
+                        key={title}
+                        onClick={onAnswerSelected}
+                    />
+                ))}
+            </div>
+        </div>
+    );
 }
 
 Turn.propTypes = {
-  author: PropTypes.shape({
-    name: PropTypes.string.isRequired,
-    imageUrl: PropTypes.string.isRequired,
-    imageSource: PropTypes.string.isRequired,
-    books: PropTypes.arrayOf(PropTypes.string).isRequired
-  }),
-  books: PropTypes.arrayOf(PropTypes.string).isRequired,
-  onAnswerSelected: PropTypes.func.isRequired,
-  highlight: PropTypes.string.isRequired
+    author: PropTypes.shape({
+        name: PropTypes.string.isRequired,
+        imageUrl: PropTypes.string.isRequired,
+        imageSource: PropTypes.string.isRequired,
+        books: PropTypes.arrayOf(PropTypes.string).isRequired,
+    }),
+    books: PropTypes.arrayOf(PropTypes.string).isRequired,
+    onAnswerSelected: PropTypes.func.isRequired,
+    highlight: PropTypes.string.isRequired,
 };
 
 function Continue({ show, onContinue }) {
-  return (
-    <div className="row continue">
-      {show ? (
-        <div className="col-11">
-          <button
-            className="btn btn-primary btn-lg float-right"
-            onClick={onContinue}
-          >
-            Continue
-          </button>
+    return (
+        <div className="row continue">
+            {show ? (
+                <div className="col-11">
+                    <button
+                        className="btn btn-primary btn-lg float-right"
+                        onClick={onContinue}
+                    >
+                        Continue
+                    </button>
+                </div>
+            ) : null}
         </div>
-      ) : null}
-    </div>
-  );
+    );
 }
 
 function Footer() {
-  return (
-    <div id="footer" className="row">
-      <div className="col-12">
-        <p className="text-muted credit">
-          All Images are from{" "}
-          <a href="https://commons.wikimedia.org/wiki/Main_Page">
-            Wikimedia Commons
-          </a>{" "}
-          and are in the public domain.
-        </p>
-      </div>
-    </div>
-  );
+    return (
+        <div id="footer" className="row">
+            <div class="col-10 offset-1">
+                <p className="text-muted credit">
+                    All Images are from{" "}
+                    <a href="https://commons.wikimedia.org/wiki/Main_Page">
+                        Wikimedia Commons
+                    </a>{" "}
+                    and are in the public domain.
+                </p>
+            </div>
+        </div>
+    );
 }
 
 function mapStateToProps(state) {
-  return {
-    turnData: state.turnData,
-    highlight: state.highlight
-  };
+    return {
+        turnData: state.turnData,
+        highlight: state.highlight,
+    };
 }
 
 function mapDispatchToProps(dispatch) {
-  return {
-    onAnswerSelected: answer => {
-      dispatch({ type: "ANSWER_SELECTED", answer });
-    },
-    onContinue: () => {
-      dispatch({ type: "CONTINUE" });
-    }
-  };
+    return {
+        onAnswerSelected: answer => {
+            dispatch({ type: "ANSWER_SELECTED", answer });
+        },
+        onContinue: () => {
+            dispatch({ type: "CONTINUE" });
+        },
+    };
 }
 
 const AuthorQuiz = connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(function({ turnData, highlight, onAnswerSelected, onContinue }) {
-  return (
-    <div className="container-fluid">
-      <Hero />
-      <Turn
-        {...turnData}
-        highlight={highlight}
-        onAnswerSelected={onAnswerSelected}
-      />
-      <Continue show={highlight === "correct"} onContinue={onContinue} />
-      <p>
-        <Link to="/add">Add an Author</Link>
-      </p>
-      <Footer />
-    </div>
-  );
+    mapStateToProps,
+    mapDispatchToProps
+)(function ({ turnData, highlight, onAnswerSelected, onContinue }) {
+    return (
+        <div className="container-fluid">
+            <Hero />
+            <Turn
+                {...turnData}
+                highlight={highlight}
+                onAnswerSelected={onAnswerSelected}
+            />
+            <Continue show={highlight === "correct"} onContinue={onContinue} />
+            <div class="row">
+                <div class="col-10 offset-1">
+                    <p>
+                        <Link to="/add">Add an Author</Link>
+                    </p>
+                </div>
+            </div>
+            <Footer />
+        </div>
+    );
 });
 export default AuthorQuiz;
